0c92e7426536c1f0ce1c5292fb06b05c202ccc3d
Doanh <doanh@pdx.edu>
2017-02-16 16:37:34 -0800

    Adding Login and session

--- FirstViewController.swift
+++ FirstViewController.swift
@@ -10,33 +10,216 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet weak var logInButtom: UIButton!
-   
-    @IBOutlet weak var name: UITextField!
+    let login_url = "http://row52-beta.azurewebsites.net/Api/V1/Account/Authenticate"
+    let checksession_url = "http://row52-beta.azurewebsites.net/Api/V1/Account/TokenIsValid"
     
-    @IBOutlet weak var password: UITextField!
+    var login_session:String = ""
+    
+   
+    @IBOutlet weak var username_input: UITextField!
+  
+    @IBOutlet weak var password_input: UITextField!
     
     @IBOutlet weak var status: UILabel!
     
+    @IBOutlet weak var login_button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        logInButtom.backgroundColor = .clear
-        logInButtom.layer.cornerRadius = 5
-        logInButtom.layer.borderWidth = 1
-        logInButtom.layer.borderColor = UIColor.black.cgColor
+        //login_button.backgroundColor = .clear
+        login_button.layer.cornerRadius = 5
+        //logInButtom.layer.borderWidth = 1
+        login_button.layer.borderColor = UIColor.white.cgColor
         
-    
+        //username_input.text = "spencer@row52.com"
+        //password_input.text = "password1"
+        //login_now(username: "tochiton", password: "password")
+        
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "session") != nil{
+            login_session = preferences.object(forKey: "session") as! String
+            check_session()
+        }
+        else{
+           LoginToDo()
+        }
+        
+        
     }
 
+    @IBAction func DoLogin(_ sender: Any) {
+        
+        if(login_button.titleLabel?.text == "Logout"){
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            LoginToDo()
+        }
+        else{
+            login_now(username: username_input.text!, password: password_input.text!)
+        }
+        
+        
+    }
+    
+    func login_now(username: String, password: String){
+        //print("\(username) is login now...")
+    
+        let post_data: NSDictionary = NSMutableDictionary()
+    
+        //TODO: add check for input username and password
+        post_data.setValue(username, forKey: "Username")
+        post_data.setValue(password, forKey: "Password")
+        
+        let url:URL = URL(string: login_url)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        var paramString = ""
+        
+        for(key, value) in post_data{
+            paramString = paramString + (key as! String) + "=" + (value as! String) + "&"
+        }
+        
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) in
+            
+            //print("Got response\(response)")
+            guard let _:Data = data, let _:URLResponse = response, error == nil else{
+                return
+            }
+            
+            let json: Any?
+            do{
+                
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print(json)
+            }
+            catch{
+                return
+            }
+            guard let server_response = json as? NSDictionary else{
+                return
+            }
+            if let data_block = server_response["Set-Cookie"] as? NSDictionary{
+                print("the cookie is: ")
+                print(data_block)
+                if let session_data = data_block["session"] as? String{
+                    self.login_session = session_data
+                    
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data, forKey: "session")
+                    DispatchQueue.main.async(execute: self.LoginDone)
+                }
+            }
+        })
+        task.resume()
+    
+    }
+    
+    func LoginDone(){
+        username_input.isEnabled = false
+        password_input.isEnabled = false
+        
+        login_button.isEnabled = true
+        
+        login_button.setTitle("Logout", for: .normal)
+        
+    }
+    func LoginToDo(){
+        username_input.isEnabled = true
+        password_input.isEnabled = true
+        
+        login_button.isEnabled = true
+        
+        login_button.setTitle("Login", for: .normal)
+        
+    }
+    func check_session()
+    {
+        let post_data: NSDictionary = NSMutableDictionary()
+        
+        
+        post_data.setValue(login_session, forKey: "session")
+        
+        let url:URL = URL(string: checksession_url)!
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+        var paramString = ""
+        
+        
+        for (key, value) in post_data
+        {
+            paramString = paramString + (key as! String) + "=" + (value as! String) + "&"
+        }
+        
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (
+            data, response, error) in
+            
+            guard let _:Data = data, let _:URLResponse = response  , error == nil else {
+                
+                return
+            }
+            
+            
+            let json: Any?
+            
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            }
+            catch
+            {
+                return
+            }
+            
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            if let response_code = server_response["response_code"] as? Int
+            {
+                if(response_code == 200)
+                {
+                    DispatchQueue.main.async(execute: self.LoginDone)
+                    
+                    
+                }
+                else
+                {
+                    DispatchQueue.main.async(execute: self.LoginToDo)
+                }
+            }
+            
+            
+            
+        })
+        
+        task.resume()
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+   
     /*
     // MARK: - Navigation
 
@@ -48,3 +231,10 @@ class FirstViewController: UIViewController {
     */
 
 }
+
+
+
+
+
+
+
